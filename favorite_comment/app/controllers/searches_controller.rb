08903e59b00edf5bc6c8ce8cb2fbf6422f048f32
Oyamada-2103 <ta9.syukatu@gmail.com>
2021-04-06 07:06:05 +0000

    [finish]kadai5

--- favorite_comment/app/controllers/searches_controller.rb
+++ favorite_comment/app/controllers/searches_controller.rb
@@ -1,14 +1,14 @@
 class SearchesController < ApplicationController
   def search
     @range = params[:range]
-    search = params[:search]
-    word = params[:word]
+    @search = params[:search]
+    @word = params[:word]
 
     # 選択された範囲で条件分岐
     if @range == "User"
-      @users = User.looks(search, word)
+      @users = User.looks(@search, @word)
     else
-      @books = Book.looks(search, word)
+      @books = Book.looks(@search, @word)
     end
   end
 end
