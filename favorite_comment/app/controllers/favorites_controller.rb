28a957af535f6d12606483444f5a98ce1305a22a
Oyamada-2103 <ta9.syukatu@gmail.com>
2021-04-11 04:45:25 +0000

    [save]kadai6_favorite_ok

--- favorite_comment/app/controllers/favorites_controller.rb
+++ favorite_comment/app/controllers/favorites_controller.rb
@@ -1,17 +1,24 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :book_params
 
   def create
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_to request.referer
+    # redirect_to request.referer
+    # 非同期通信機能実装のため削除
   end
 
   def destroy
     @book = Book.find(params[:book_id])
     favorite = @book.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    redirect_to request.referer
+    # redirect_to request.referer
+    # 非同期通信機能実装のため削除
+  end
+
+  private
+  def book_params
+    @book = Book.find(params[:book_id])
   end
 end
